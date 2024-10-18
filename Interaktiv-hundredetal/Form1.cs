c66b499b5f7cecf24f055a6db165aa1aa902c1e4
Jacobhbo <111510486+Jacobhbo@users.noreply.github.com>
2024-10-18 09:52:04 +0200

    final

--- Interaktiv-hundredetal/Form1.cs
+++ Interaktiv-hundredetal/Form1.cs
@@ -4,7 +4,10 @@ namespace Interaktiv_hundredetal
 {
     public partial class Form1 : Form
     {
+        // En offentlig variabel, der gemmer et tilfældigt genereret tal, som bruges i beregningerne.
         public int randomNumber;
+
+        //Dette er konstruktøren for klassen Form1, der initialiserer GUI-komponenterne
         public Form1()
         {
             InitializeComponent();
@@ -17,10 +20,13 @@ namespace Interaktiv_hundredetal
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Opretter en ny instans af Random-klassen til at generere tilfældige tal
             Random random = new Random();
 
+            // Genererer et tilfældigt tal mellem 1 og 100 (inklusive) og gemmer det i variablen randomNumber
             randomNumber = random.Next(1, 101);
 
+            // Sætter tekstboksen textBoxTallet's tekst til det tilfældige tal som en streng
             textBoxTallet.Text = randomNumber.ToString();
 
         }
@@ -32,15 +38,17 @@ namespace Interaktiv_hundredetal
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Forsøger at konvertere tekstboksen (textBox9) til en integer (value9) og tjekker om den er lig med randomNumber + 10
             if (int.TryParse(textBox9.Text, out int value9) && value9 == randomNumber + 10)
             {
+                
                 textBox9.BackColor = Color.Green;
             }
             else
             {
                 textBox9.BackColor = Color.Red;
             }
-
+            
             if (int.TryParse(textBox4.Text, out int value4) && value4 == randomNumber * 10)
             {
                 textBox4.BackColor = Color.Green;
@@ -135,22 +143,7 @@ namespace Interaktiv_hundredetal
 
         private void textBox9_TextChanged(object sender, EventArgs e)
         {
-            //+10
-            //if (int.TryParse(textBox9.Text, out int userInput))
-            //{
-            //    if (userInput == randomNumber + 10)
-            //    {
-            //        textBox9.BackColor = Color.Green;
-            //    }
-            //    else
-            //    {
-            //        textBox9.BackColor = Color.Red;
-            //    }
-            //}
-            //else
-            //{
-            //    textBox9.BackColor = Color.White;
-            //}
+            
         }
 
         private void textBox7_TextChanged(object sender, EventArgs e)
@@ -180,8 +173,10 @@ namespace Interaktiv_hundredetal
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // Opretter en liste af tekstbokse, som skal tømmes og nulstilles
             List<TextBox> textBoxes = new List<TextBox> { textBox1, textBox2, textBox4, textBox5, textBox6, textBox7, textBox8, textBox9 };
 
+            // Rydder (fjerner) teksten i hver tekstboks ved at kalde Clear()-metoden
             textBox1.Clear();
             textBox2.Clear();
             textBox4.Clear();
@@ -191,8 +186,10 @@ namespace Interaktiv_hundredetal
             textBox8.Clear();
             textBox9.Clear();
 
+            // Gennemgår alle tekstbokse i listen
             foreach (var textBox in textBoxes)
             {
+                // Hvis tekstboksen er tom (efter at Clear() er blevet kaldt), sættes baggrundsfarven til hvid
                 if (string.IsNullOrEmpty(textBox.Text))
                 {
                     textBox.BackColor = Color.White;
